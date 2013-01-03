22e319af18d7f1c09dc2a0db67143c16ab4cc2f9
Gregory Eric Sanderson <gzou2000@gmail.com>
2013-01-03 18:48:56 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from greglink import app
+
+app.config.from_object('greglink.default_config')
+app.config.from_envvar('CONFIG_FILE')
+
+if __name__ == "__main__":
+    port = int(app.config['PORT'])
+    debug = int(app.config['DEBUG'])
+    host = app.config['HOST']
+    app.run(host=host, port=port, debug=debug)
