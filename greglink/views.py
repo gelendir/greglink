cb2ff266093708b282e6dcbd4473871e206e9073
Gregory Eric Sanderson <gzou2000@gmail.com>
2013-01-03 20:58:26 -0500

    add views for executing a test

--- greglink/views.py
+++ greglink/views.py
@@ -1,6 +1,47 @@
-from greglink import app
+import os
+
+from greglink import app, lib, models
 from flask import render_template, request, redirect, url_for
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/<id>/execute')
+def execute_test(id):
+    testcase = lib.find_test(id)
+    if not testcase:
+        return ("Test not found", 404)
+
+    return render_template('execute_test.html', testcase=testcase)
+
+@app.route('/<id>/passed')
+def test_success(id):
+    testcase = lib.find_test(id)
+    if not testcase:
+        return ("Test not found", 404)
+
+    models.test_success(testcase)
+
+    return "test passed"
+
+@app.route('/<id>/blocked')
+def test_blocked(id):
+    testcase = lib.find_test(id)
+    if not testcase:
+        return ("Test not found", 404)
+
+    models.test_blocked(testcase)
+
+    return "test blocked"
+
+@app.route('/<id>/failed')
+def test_failed(id):
+    testcase = lib.find_test(id)
+    if not testcase:
+        return ("Test not found", 404)
+
+    models.test_failed(testcase)
+
+    return "test failed"
