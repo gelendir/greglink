d1c77dd5627f39bb7bb5e743e845e21ee1cf37fc
Gregory Eric Sanderson <gzou2000@gmail.com>
2013-01-06 18:41:33 -0500

    seperate test id from database id, add timestamp to test execution

--- greglink/models/test_execution.py
+++ greglink/models/test_execution.py
@@ -1,11 +1,18 @@
 from greglink import db
+from datetime import datetime
+
+def datetime_utc_now():
+    return datetime.utcnow()
 
 class TestExecution(db.Model):
 
     __tablename__ = 'test_executions'
 
-    id = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    test_id = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime_utc_now)
     status_id = db.Column(db.Integer, db.ForeignKey('statuses.id'))
+
     status = db.relationship('Status')
 
     def __repr__(self):
