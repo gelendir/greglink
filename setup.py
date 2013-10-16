ae91e89ef28bb0f48540cbb027313c0d34db34ed
Sebastien Duthil <duthils@free.fr>
2013-10-15 21:44:29 -0400

    fix broken import and README

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from greglink import app
-from greglink import database
+from greglink.lib import database
 
 app.config.from_object('greglink.config.default_config')
 app.config.from_envvar('CONFIG_FILE')
